57e381044ee8525cd0af4dc98ccd6e6955ca8b96
paulonmachado <paulonmachado@me.com>
2020-07-17 02:56:42 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Poodle from "../views/Poodle.vue";
 import Finder from "../views/Finder.vue";
-import Quem from "../views/Quem.vue";
 
 Vue.use(VueRouter);
 
